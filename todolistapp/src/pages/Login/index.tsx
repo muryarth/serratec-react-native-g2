881887a296c10ac43eaba85e6bffacbb53a13eaa
BernardoMeira <Bernardomeira@live.com>
2024-06-24 19:58:03 -0300

    feat: Subindo pagina Login

--- todolistapp/src/pages/Login/index.tsx
+++ todolistapp/src/pages/Login/index.tsx
@@ -1,21 +1,140 @@
-import { Text, SafeAreaView } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { Text, SafeAreaView, Keyboard, Alert, View } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import Icon from "@expo/vector-icons/Ionicons";
+import { styles } from "./style";
+import { TextInputComponent } from "../../components/TextInputComponent";
+import { DefaultAppButton } from "../../components/DefaultAppButton";
 
 const Login = () => {
+  const [email, setEmail] = useState<string>();
+  const [senha, setSenha] = useState<string>();
+
   const navigator = useNavigation();
 
+  const placeholderColor = "#000";
+
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [senhaError, setSenhaError] = useState<boolean>(false);
+
+  const handleEmail = (value: string) => {
+    setEmail(value);
+  };
+
+  const handleSenha = (value: string) => {
+    setSenha(value);
+  };
+
+  const handleCadastro = () => {
+    navigator.navigate("StackCadastro");
+  };
+
+  const validateEmail = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const handleErrors = () => {
+    let emailErr = false;
+    let senhaErr = false;
+
+    if (!email || !validateEmail(email)) {
+      emailErr = true;
+    }
+
+    if (!senha) {
+      senhaErr = true;
+    }
+
+    setEmailError(emailErr);
+    setSenhaError(senhaErr);
+
+    if (!emailErr && !senhaErr) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // const handleLogin = async () => {
+  //   if (!handleErrors()) {
+  //     try {
+  //       const response = await axios.post("http://10.0.2.2:8080/login", {
+  //         email,
+  //         senha,
+  //       });
+
+  //       if (response.status === 200) {
+  //         Alert.alert("Login realizado!");
+
+  //         navigator.navigate("StackHome");
+  //       } else {
+  //         Alert.alert(
+  //           "Login não realizado!",
+  //           response.data.message || "Aconteceu algum erro..."
+  //         );
+  //       }
+  //     } catch (error: any) {
+  //       if (error.response) {
+  //         if (error.response.status === 401) {
+  //           Alert.alert("Login não realizado!", error.response.data);
+  //         }
+  //       } else {
+  //         Alert.alert("Erro na requisição!", "Aconteceu um erro inesperado.");
+  //         console.log("Erro na requisição!", error);
+  //       }
+  //     }
+  //   }
+  // };
+
   const handleLogin = () => {
-    navigator.navigate("StackHome");
+    if (!handleErrors()) {
+      navigator.navigate("StackHome");
+    }
   };
 
   return (
     <SafeAreaView
       style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
-      <TouchableOpacity onPress={handleLogin}>
-        <Text>Ir para home.</Text>
-      </TouchableOpacity>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <View style={styles.textinput}>
+            <View style={styles.containerIcon}>
+              <Icon style={styles.icon} name="person-outline" size={120} />
+              <Text style={styles.title}>LOGIN</Text>
+            </View>
+
+            <TextInputComponent
+              placeholder="Digite seu email"
+              placeholderColor={placeholderColor}
+              onChangeValue={handleEmail}
+              error={emailError}
+              errorText="Preencha com um email valido!"
+            />
+
+            <TextInputComponent
+              onChangeValue={handleSenha}
+              placeholderColor={placeholderColor}
+              placeholder="Digite sua senha"
+              type={true}
+              error={senhaError}
+              errorText="Preencha a senha!"
+            />
+          </View>
+
+          <View style={styles.buttons}>
+            <DefaultAppButton titulo="Entrar" customOnPress={handleLogin} />
+
+            <DefaultAppButton
+              titulo="Cadastrar"
+              customOnPress={handleCadastro}
+            />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 };
---
+++ todolistapp/src/pages/Login/style.ts
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  containerIcon: {
+    alignItems: "center",
+    // backgroundColor: "black",
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: "500",
+    color: "#ccc",
+    marginBottom: 10,
+  },
+
+  icon: {
+    color: "#ccc",
+  },
+
+  textinput: {
+    marginTop: 130,
+    width: "95%",
+    alignItems: "center",
+  },
+
+  buttons: {
+    width: '90%',
+    marginBottom: 100,
+  },
+});
