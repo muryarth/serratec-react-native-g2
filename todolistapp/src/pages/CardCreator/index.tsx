27f2e708a090af931b7dbb9e87ec45cd33cd943b
Rafaela Beber <rafaelabebercoelho@gmail.com>
2024-06-24 20:35:39 -0300

    feat: add funcao de criar card

--- todolistapp/src/pages/CardCreator/index.tsx
+++ todolistapp/src/pages/CardCreator/index.tsx
@@ -1,14 +1,56 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { styles } from "./style";
 import { DefaultAppButton } from "../../components/DefaultAppButton";
+import axios from "axios";
+import { useState } from "react";
+import { ModalComponent } from "../../components/ModalComponent";
+
 const CardCreator = () => {
+  const [titulo, setTitulo] = useState('');
+  const [descricao, setDescricao] = useState('')
+  const [modalComponent, setModalComponent] = useState(false)
+  
+
+  const handleCardCreator = async () => {
+    try {
+      const data ={
+        titulo: titulo,
+        descricao: descricao,
+      };
+
+      await axios.post ("https://6674c60975872d0e0a977f85.mockapi.io/lembrete", data)
+      setModalComponent(true)
+      setTitulo('');
+      setDescricao('');
+    }catch (error){
+      
+      console.error(error);
+    }
+    
+  };
+
   return (
     <View style={styles.cardText}>
       <View style={styles.cardContainer}>
-        <TextInput placeholder="Titulo"></TextInput>
-      </View>
-      <TextInput multiline style ={styles.cardTextInput} placeholder="Conteudo do Card"></TextInput >
-      <DefaultAppButton titulo="Criar" customOnPress={()=>console.log("Clicou")}/>
+        
+    <TextInput
+      placeholder="Insira um Título"
+      value={titulo}
+      onChangeText={setTitulo}
+
+    />
+    </View>
+      <TextInput multiline 
+      style ={styles.cardTextInput} 
+      placeholder="Insira o Conteúdo do Card"
+      value={descricao}
+      onChangeText={setDescricao}
+      />
+      
+      <DefaultAppButton titulo="Criar" customOnPress={handleCardCreator}/>
+
+      <ModalComponent visible={modalComponent} handleClose ={() => setModalComponent(false)}
+     modalTitle={"Card criado com sucesso!"}/>
     </View>
   );
 };
